0dc7fafef0b4ba3d4dab228dc1d32f4a2ded6374
Ibrahim Kk <kkibrahim008@gmail.com>
2020-04-13 15:53:01 +0500

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+/**
+ * Created by Asus on 23/02/2018.
+ */
+
+class constants {
+  static String APP_ID = "bdb1e6f19ebc431db6ae889061f42afa";
+  static String APP_SECRET = "305ae5581b4f494c93571a231445fbfb";
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Module/Login/login_view.dart';
+
+void main() => runApp(new MaterialApp(
+    title: 'Instagram Login',
+    theme: new ThemeData(primarySwatch: Colors.blue),
+    home: new LoginPage()));
